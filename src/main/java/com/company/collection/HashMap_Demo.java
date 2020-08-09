a7ece4956697d2d43687917001ca36714243e4db
asus <asu9421@gmail.com>
2020-08-09 22:05:03 +0530

    Streams started

--- src/main/java/com/company/collection/HashMap_Demo.java
+++ src/main/java/com/company/collection/HashMap_Demo.java
@@ -7,51 +7,44 @@ import java.util.stream.Collectors;
 
 public class HashMap_Demo {
 
-    @Test
-    public void demo1() {
+    public Map<Integer, String> getMap() {
         Map<Integer, String> hm = new HashMap<>();
         hm.put(1, "A");
         hm.put(2, "B");
         hm.put(3, "C");
         hm.put(4, "D");
         hm.put(5, "E");
+        return hm;
+    }
 
-        hm.get(1);
+    @Test
+    public void demo1() {
+        Map<Integer, String> hm = getMap();
         for (Map.Entry m : hm.entrySet()) {
             System.out.println(m.getKey() + " " + m.getValue());
         }
         // using java 8
-        hm.forEach((a,b)-> System.out.println(a+":"+b));
+        hm.forEach((a, b) -> System.out.println(a + ":" + b));
     }
 
     @Test
-    public void demo2(){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        map.put(1, "A");
-        map.put(2, "B");
-        map.put(3, "C");
-        map.put(4, "D");
-        map.put(5, "E");
+    public void demo2() {
+        Map<Integer, String> map = getMap();
         /* Display content using Iterator*/
         Set<Map.Entry<Integer, String>> set = map.entrySet();
         Iterator iterator = set.iterator();
-        while(iterator.hasNext()) {
-            Map.Entry entry = (Map.Entry)iterator.next();
-            System.out.print("key is: "+ entry.getKey() + " & Value is: ");
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry) iterator.next();
+            System.out.print("key is: " + entry.getKey() + " & Value is: ");
             System.out.println(entry.getValue());
         }
         System.out.println("==============================");
-        map.forEach((a,b)-> System.out.println(a+":"+b));
+        map.forEach((a, b) -> System.out.println(a + ":" + b));
     }
 
     @Test
-    public void iterating_1(){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        map.put(1, "A");
-        map.put(2, "B");
-        map.put(3, "C");
-        map.put(4, "D");
-        map.put(5, "E");
+    public void iterating_1() {
+        Map<Integer, String> map = getMap();
         /*Iterator it = mp.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
@@ -68,7 +61,7 @@ public class HashMap_Demo {
             System.out.println(entry.getValue());
         }*/
         System.out.println("==============================");
-        map.forEach((a,b)-> System.out.println(a+":"+b));
+        map.forEach((a, b) -> System.out.println(a + ":" + b));
     }
 
     @Test
@@ -117,8 +110,6 @@ public class HashMap_Demo {
                                 (e1, e2) -> e1, LinkedHashMap::new));
         System.out.println(sortedMap);
     }
-
-
 
 
 }
