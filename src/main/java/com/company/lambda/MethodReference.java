8334215d29aa5a653959119f8f4957ac78314dd8
asus <asu9421@gmail.com>
2021-03-21 12:10:27 +0530

    updated

--- src/main/java/com/company/lambda/MethodReference.java
+++ src/main/java/com/company/lambda/MethodReference.java
@@ -10,9 +10,8 @@ import java.util.stream.IntStream;
 
 public class MethodReference {
 
-    //Traditional approach
+    //Traditional approach: just for understanding, logic is not full proff
     private static boolean isPrime(int number) {
-        if (number < 2) return false;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) return false;
         }
@@ -27,15 +26,21 @@ public class MethodReference {
     }*/
 
     @Test
-    public void test_01() {
+    public void test_withoutMethodRef() {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 4, 6, 8, 9, 17, 23));
         List<Integer> collect = numbers.stream()
-                .filter(number -> number > 2)
                 .filter(number -> IntStream.range(2, number).noneMatch(index -> number % index == 0))
                 .collect(Collectors.toList());
-
         System.out.println(collect);
     }
 
+    @Test
+    public void test_withMethodRef() {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 4, 6, 8, 9, 17, 23));
+        List<Integer> collect = numbers.stream()
+                .filter(MethodReference::isPrime)
+                .collect(Collectors.toList());
+        System.out.println(collect);
+    }
 
 }
