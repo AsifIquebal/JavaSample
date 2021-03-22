f206ab5d2890b64a19e1eaa11108158c21465725
asus <asu9421@gmail.com>
2021-03-22 13:33:40 +0530

    updated

---
+++ src/main/java/com/company/constructor/Initials.java
@@ -0,0 +1,16 @@
+package com.company.constructor;
+
+public class Initials {
+
+    int x;
+
+    public int Initials(){
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Initials initials = new Initials();
+
+    }
+
+}
