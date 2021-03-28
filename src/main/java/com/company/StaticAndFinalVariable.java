57370e91806f78e84f22352af00e11e4fada65a2
asus <asu9421@gmail.com>
2021-03-28 22:02:34 +0530

    updated

--- src/main/java/com/company/StaticAndFinalVariable.java
+++ src/main/java/com/company/StaticAndFinalVariable.java
@@ -1,19 +1,18 @@
 package com.company;
 
 public class StaticAndFinalVariable {
-    public static final int x =10;
-
     public static void main(String[] args) {
-        System.out.println(x);
+        System.out.println(Hello.x);
+        System.out.println(Hello.name);
     }
-
 }
-
-
-
-/*class Hello {
-    final static int x=10;
+// even if static block executes at the time of class loading, its needs a trigger
+// which in this case is when we are accessing the instance static variable
+// the final varibale is in-line, so already available to compiler
+class Hello {
+    public final static int x = 10;
+    public static String name = "Suja";
     static {
         System.out.println("Hello");
     }
-}*/
+}
