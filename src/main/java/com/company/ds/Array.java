42eedce7d7ab0ef7d7d5cb79de0ed878830c1b39
asus <asu9421@gmail.com>
2021-04-03 03:05:05 +0530

    updated

--- src/main/java/com/company/ds/Array.java
+++ src/main/java/com/company/ds/Array.java
@@ -7,23 +7,62 @@ import java.util.Set;
 
 public class Array {
 
+    // Brute force
+    // Traversing the array, 2 loops, time O(n^2)
+
+    // Set: traversing time O(n)
+    // worst case space O(n)
+
+
     // Find duplicates in O(n) and O(1) for a given array of n elements
-    // elements can be in 0 to n-1
+    // elements can be in 1 to n-1
     // Encoding: O(N) and O(1)
     @Test
     public void test_01() {
-        int[] a = {1, 2, 2, 1,2,6,6,7};
+        int[] a = {1, 2, 3, 1, 3, 3, 1, 6, 6};
+        //int[] a = {3,1,3}; // invalid, not in 1 to n-1
+
         Set<Integer> result = new HashSet<>();
         for (int i = 0; i < a.length; i++) {
             int index = Math.abs(a[i]);
             if (a[index] > 0) {
                 a[index] = -a[index];
-            }
-            else {
+            } else {
                 result.add(Math.abs(a[i]));
             }
         }
         System.out.println(result);
+    }
+
+
+    /* Problem Statement:
+     * an array of n+1 integers between 1 and n [inclusive]
+     * only one duplicate, may be repeated several times
+     * 1,3,4,2,2 => 2
+     * 3,1,3,4,2 => 3
+     * No modification of the array allowed
+     * Floyd Cycle Method/ Tortoise Hare Method
+     * Time: O(n) space: O(1)
+     * */
+
+    @Test
+    public void test_02() {
+        int[] a = {5, 3, 2, 4, 2,1};
+        int slow = a[0];
+        int fast = a[0];
+
+        do {
+            slow = a[slow];
+            fast = a[a[fast]];
+        } while (slow != fast);
+
+        fast = a[0];
+        while (slow != fast) {
+            slow = a[slow];
+            fast = a[fast];
+            System.out.println("slow: " + slow + ", fast: " + fast);
+        }
+        System.out.println("Duplicate: " + fast);
     }
 
 }
