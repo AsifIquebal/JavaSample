6860c190b7e07d6bb05e3dd8ff8e54cf7bd8955a
asus <asu9421@gmail.com>
2021-04-25 00:51:23 +0530

    ..

--- src/main/java/com/company/ds/sortingSearching/BinarySearch.java
+++ src/main/java/com/company/ds/sortingSearching/BinarySearch.java
@@ -11,9 +11,22 @@ public class BinarySearch {
         int right = a.length - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
+            //System.out.println(mid);
             if (a[mid] == target) return mid;
             else if (a[mid] < target) left = mid + 1;
             else if (a[mid] > target) right = mid - 1;
+        }
+        return -1;
+    }
+
+    public int searchRecursively(int[]a, int left, int right, int target){
+        int mid = (left+right)/2;
+        if(target == a[mid]){
+            return mid;
+        }else if(target < a[mid]){
+            return searchRecursively(a,left, mid-1, target);
+        }else if(target > a[mid]){
+            return searchRecursively(a,mid+1, right, target);
         }
         return -1;
     }
@@ -25,6 +38,7 @@ public class BinarySearch {
         System.out.println("Sorted Array: " + Arrays.toString(array));
         int i = search(array,7);
         System.out.println(array[i] + ":" + i);
+        System.out.println(searchRecursively(array,0,array.length-1, 7));
     }
 
 }
