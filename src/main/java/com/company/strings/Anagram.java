88b839621a2f1664c9353a64de2826b4f1d6bb68
asus <asu9421@gmail.com>
2021-08-29 11:03:34 +0530

    ..

--- src/main/java/com/company/strings/Anagram.java
+++ src/main/java/com/company/strings/Anagram.java
@@ -1,13 +1,32 @@
 package com.company.strings;
 
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Anagram {
 
-    static void isAnagram(String str1, String str2) {
-        String s1 = str1.replaceAll("\\s", "");
-        String s2 = str2.replaceAll("\\s", "");
-        boolean status = true;
+
+    @Test
+    public void solution01() {
+        isAnagram("Keep", "Peek");
+        isAnagram("Mother In Law", "Hitler Woman");
+        isAnagram("A decimal point", "I'm a dot in place");
+    }
+
+    @Test
+    public void solution02() {
+        isAnagram02("A decimal point", "I'm a dot in place");
+        isAnagram02("abc", "acd");
+    }
+
+
+    private void isAnagram(String str1, String str2) {
+        String s1 = str1.replaceAll("\\s", "").replaceAll("'", "");
+        String s2 = str2.replaceAll("\\s", "").replaceAll("'", "");
+        boolean status;
         if (s1.length() != s2.length()) {
             status = false;
         } else {
@@ -16,7 +35,6 @@ public class Anagram {
             Arrays.sort(ArrayS1);
             Arrays.sort(ArrayS2);
             System.out.println(ArrayS1[0]);
-            //Arrays.sort(new char[][]{ArrayS1}, Collections.reverseOrder());
             status = Arrays.equals(ArrayS1, ArrayS2);
         }
         if (status) {
@@ -26,16 +44,25 @@ public class Anagram {
         }
     }
 
-    public static void main(String[] args) {
-        isAnagram("Keep", "Peek");
-        isAnagram("Mother In Law", "Hitler Woman");
+    private void isAnagram02(String str1, String str2) {
+        String s1 = str1.toLowerCase().replaceAll("\\s", "").replaceAll("'", "");
+        String s2 = str2.toLowerCase().replaceAll("\\s", "").replaceAll("'", "");
+        List<Character> list = new ArrayList<>();
+        if (s1.length() != s2.length()) {
+            System.out.println("Length is not matching");
+        } else {
+            System.out.println("Before adding up: " + list);
+            s1.chars().mapToObj(c -> (char) c).forEach(list::add);
+            System.out.println("After adding up: " + list);
+            s2.chars().mapToObj(c -> (char) c).forEach(list::remove);
+            System.out.println("After removing: " + list);
+            System.out.println(list);
+            if (list.isEmpty()) {
+                System.out.println(str1 + " and " + str2 + ", are anagrams");
+            } else {
+                System.out.println(str1 + " and " + str2 + ", are not anagrams");
+            }
+        }
     }
+
 }
-
-
-
-
-
-
-
-
