c1db4a265f6cf1d4ba2536a57b1c154c4b20bc7f
asus <asu9421@gmail.com>
2021-10-17 12:01:28 +0530

    test

---
+++ src/main/java/com/company/commonlyasked/regex/RegularExpresion_1.java
@@ -0,0 +1,57 @@
+package com.company.commonlyasked.regex;
+
+import org.testng.annotations.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpresion_1 {
+    public static void main(String args[]) {
+        // String to be scanned to find the pattern.
+        String line = "This order was placed for QT3000! OK?";
+        String pattern = "(.*)(\\d+)(.*)";
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern);
+        // Now create matcher object.
+        Matcher m = r.matcher(line);
+
+        System.out.println("Total Groups present [groupCount method]: " + m.groupCount());
+        if (m.find()) {
+            System.out.println("Found value: " + m.group(0));
+            System.out.println("Found value: " + m.group(1));
+            System.out.println("Found value: " + m.group(2));
+
+
+        } else {
+            System.out.println("NO MATCH");
+        }
+    }
+
+    @Test
+    public void test2() {
+        String a = "Asif(01)";
+        String b = a.replaceAll("[^a-zA-Z]", "");
+        System.out.println(b);
+        System.out.println("Gaurav J(01)".replaceAll("[^a-zA-Z]", ""));
+        System.out.println("Gaurav J(01)".replaceAll("[^a-zA-Z\\s]", ""));
+    }
+
+    @Test
+    public void test3() {
+        Pattern p = Pattern.compile("-?\\d+");
+        Matcher m = p.matcher("There are more than -2 and less than 12 numbers here");
+        int i = 0;
+        while (m.find()) {
+            System.out.println(i);
+            System.out.println(m.group());
+            i++;
+        }
+    }
+
+    @Test
+    public void maskAllExceptLast4Characters(){
+        String str = "4DR534D&234dfrt234";
+        System.out.println(str.replaceAll(".(?=.{4})", "X"));
+    }
+
+}
